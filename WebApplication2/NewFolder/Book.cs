bf4df9404c4f79f14d91bfe58134fdba7e869e8c
Dhritendu jha <Dhritendu jha@LAPTOP-A61RULDB>
2022-12-11 22:56:05 +0530

    Add project files.

---
+++ WebApplication2/NewFolder/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication2.NewFolder
+{
+    public class Book
+    {   
+        public int Id { get; set; }
+      
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+}
